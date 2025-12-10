f9328f829844ab23e824a7ba1be0443d0bbc479f
Semion Shishlov <semionshish@gmail.com>
2025-12-10 23:02:53 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// Для GitHub Pages: если репозиторий называется не 'cards', 
+// Для GitHub Pages: если репозиторий называется не 'pack', 
 // измените base на '/ваше-имя-репозитория/'
 export default defineConfig({
   plugins: [react()],
   base: process.env.GITHUB_PAGES === 'true' || process.env.NODE_ENV === 'production' 
-    ? '/cards/' 
+    ? '/pack/' 
     : '/',
 })
 
